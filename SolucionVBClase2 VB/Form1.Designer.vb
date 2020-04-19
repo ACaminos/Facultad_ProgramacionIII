dbfd0cc47a6117dc16dd8f7fef6ea195b88900ca
Ariel Caminos <caminosariel@gmail.com>
2020-04-18 23:46:42 -0300

    Add files via upload

---
+++ SolucionVBClase2 VB/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.bBuscar = New System.Windows.Forms.Button()
+        Me.bTexto = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'bBuscar
+        '
+        Me.bBuscar.Location = New System.Drawing.Point(507, 93)
+        Me.bBuscar.Name = "bBuscar"
+        Me.bBuscar.Size = New System.Drawing.Size(119, 23)
+        Me.bBuscar.TabIndex = 0
+        Me.bBuscar.Text = "Buscar"
+        Me.bBuscar.UseVisualStyleBackColor = True
+        '
+        'bTexto
+        '
+        Me.bTexto.Location = New System.Drawing.Point(223, 95)
+        Me.bTexto.Name = "bTexto"
+        Me.bTexto.Size = New System.Drawing.Size(278, 20)
+        Me.bTexto.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Bodoni MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(82, 95)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(130, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Escriba aqui ---->"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Font = New System.Drawing.Font("Bodoni MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel1.Location = New System.Drawing.Point(695, 315)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(77, 20)
+        Me.LinkLabel1.TabIndex = 3
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Link a C#"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.bTexto)
+        Me.Controls.Add(Me.bBuscar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents bBuscar As Button
+    Friend WithEvents bTexto As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+End Class
---
+++ SolucionVBClase2 VB/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub bBuscar_Click(sender As Object, e As EventArgs) Handles bBuscar.Click
+        System.Diagnostics.Process.Start("https://www.google.com/search?sxsrf=ALeKk00FM66tKMxv08qH4poIKmOxzDTxMw%3A1586371526334&source=hp&ei=xhuOXpnxEY655OUP9oej8AE&q=" & bTexto.Text & "&oq=" & bTexto.Text & " &gs_lcp=CgZwc3ktYWIQAzICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgUIABCDATICCAA6BAgjECdKGQgXEhUwZzE0M2cxNzZnMTY3ZzE3MWcxMzVKDwgYEgswZzFnMWcxZzFnMVDqDFi3FGDGF2gAcAB4AIABf4gBrQSSAQMyLjOYAQCgAQGqAQdnd3Mtd2l6&sclient=psy-ab&ved=0ahUKEwiZ7Lm6vtnoAhWOHLkGHfbDCB4Q4dUDCAY&uact=5")
+
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        System.Diagnostics.Process.Start("/Documentos/Programacion III/Navegador/bin/debug/Navegador.exe")
+    End Sub
+End Class
