cb7579c02fe187f3678961e2ee7fde479739afe6
Ariel Caminos <caminosariel@gmail.com>
2020-04-19 00:01:24 -0300

    Add files via upload

---
+++ BuscadorClase3 VB/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.BackColor = Color.Red
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.BackColor = Color.Green
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.BackColor = Color.Blue
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Button4.Visible = Not Button4.Visible
+    End Sub
+
+    Private Sub bUrl_Click(sender As Object, e As EventArgs) Handles bUrl.Click
+        If String.IsNullOrEmpty(tBuscador.Text) Then Return
+        If tBuscador.Equals("about:blank") Then Return
+        If Not tBuscador.Text.StartsWith("http://") And Not tBuscador.Text.StartsWith("https://") Then
+            tBuscador.Text = "http://" & tBuscador.Text
+        End If
+        Try
+            WebBrowser1.Navigate(New Uri(tBuscador.Text))
+        Catch
+            MessageBox.Show("Se produjo un error")
+        End Try
+    End Sub
+
+    Private Sub bBuscar_Click(sender As Object, e As EventArgs) Handles bBuscar.Click
+        System.Diagnostics.Process.Start("https://www.google.com.ar/search?sxsrf=ALeKk02D89EyZlLwfcRlnhYWPLdo3XOmng%3A1586734702291&source=hp&ei=bqaTXqiTD4G95OUPyfuIkAI&q=" + tBuscador.Text + "&oq=" + tBuscador.Text + "&gs_lcp=CgZwc3ktYWIQAzIECCMQJzICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADoFCAAQgwFKGAgXEhQyLTU3NWcxNzY5ZzE4MjlnMTg5MUoNCBgSCTItMWcxZzFnMVDumAFYwp8BYOazAWgAcAB4AYABpQ6IAdw1kgEJMi0xLjctMS4zmAEAoAEBqgEHZ3dzLXdperABAA&sclient=psy-ab&ved=0ahUKEwiogp-yh-ToAhWBHrkGHck9AiIQ4dUDCAY&uact=5")
+    End Sub
+End Class
---
+++ BuscadorClase3 VB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("BuscadorClaseII")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("BuscadorClaseII")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("f327c4a5-59a5-4cc9-83b1-4602478a7743")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
