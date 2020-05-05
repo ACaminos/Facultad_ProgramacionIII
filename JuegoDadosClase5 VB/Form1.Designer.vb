acf80ae7b5e244d02e46a92ddf568b02bc786b9b
Ariel Caminos <caminosariel@gmail.com>
2020-05-05 17:29:34 -0300

    Add files via upload

---
+++ JuegoDadosClase5 VB/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.cubilete = New System.Windows.Forms.PictureBox()
+        Me.quintoDado = New System.Windows.Forms.PictureBox()
+        Me.cuartoDado = New System.Windows.Forms.PictureBox()
+        Me.tercerDado = New System.Windows.Forms.PictureBox()
+        Me.segundoDado = New System.Windows.Forms.PictureBox()
+        Me.primerDado = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.cubilete, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.quintoDado, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.cuartoDado, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.tercerDado, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.segundoDado, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.primerDado, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'cubilete
+        '
+        Me.cubilete.BackColor = System.Drawing.Color.Transparent
+        Me.cubilete.Location = New System.Drawing.Point(474, 12)
+        Me.cubilete.Name = "cubilete"
+        Me.cubilete.Size = New System.Drawing.Size(300, 258)
+        Me.cubilete.TabIndex = 1
+        Me.cubilete.TabStop = False
+        '
+        'quintoDado
+        '
+        Me.quintoDado.BackgroundImage = Global.JuegoDadosClase5_VB.My.Resources.Resources.dado5
+        Me.quintoDado.Location = New System.Drawing.Point(36, 135)
+        Me.quintoDado.Name = "quintoDado"
+        Me.quintoDado.Size = New System.Drawing.Size(94, 95)
+        Me.quintoDado.TabIndex = 3
+        Me.quintoDado.TabStop = False
+        '
+        'cuartoDado
+        '
+        Me.cuartoDado.BackgroundImage = Global.JuegoDadosClase5_VB.My.Resources.Resources.dado4
+        Me.cuartoDado.Location = New System.Drawing.Point(410, 362)
+        Me.cuartoDado.Name = "cuartoDado"
+        Me.cuartoDado.Size = New System.Drawing.Size(94, 95)
+        Me.cuartoDado.TabIndex = 4
+        Me.cuartoDado.TabStop = False
+        '
+        'tercerDado
+        '
+        Me.tercerDado.BackgroundImage = Global.JuegoDadosClase5_VB.My.Resources.Resources.dado3
+        Me.tercerDado.Location = New System.Drawing.Point(287, 73)
+        Me.tercerDado.Name = "tercerDado"
+        Me.tercerDado.Size = New System.Drawing.Size(94, 95)
+        Me.tercerDado.TabIndex = 5
+        Me.tercerDado.TabStop = False
+        '
+        'segundoDado
+        '
+        Me.segundoDado.BackgroundImage = Global.JuegoDadosClase5_VB.My.Resources.Resources.dado2
+        Me.segundoDado.Location = New System.Drawing.Point(75, 377)
+        Me.segundoDado.Name = "segundoDado"
+        Me.segundoDado.Size = New System.Drawing.Size(94, 95)
+        Me.segundoDado.TabIndex = 6
+        Me.segundoDado.TabStop = False
+        '
+        'primerDado
+        '
+        Me.primerDado.BackgroundImage = Global.JuegoDadosClase5_VB.My.Resources.Resources.dado1
+        Me.primerDado.Location = New System.Drawing.Point(189, 216)
+        Me.primerDado.Name = "primerDado"
+        Me.primerDado.Size = New System.Drawing.Size(94, 95)
+        Me.primerDado.TabIndex = 7
+        Me.primerDado.TabStop = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(784, 24)
+        Me.MenuStrip1.TabIndex = 8
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArchivoToolStripMenuItem
+        '
+        Me.ArchivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SalirToolStripMenuItem})
+        Me.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
+        Me.ArchivoToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.ArchivoToolStripMenuItem.Text = "Archivo"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(96, 22)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(784, 503)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.primerDado)
+        Me.Controls.Add(Me.segundoDado)
+        Me.Controls.Add(Me.tercerDado)
+        Me.Controls.Add(Me.cuartoDado)
+        Me.Controls.Add(Me.quintoDado)
+        Me.Controls.Add(Me.cubilete)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Apretá el cubilete para tirar los dados"
+        CType(Me.cubilete, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.quintoDado, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.cuartoDado, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.tercerDado, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.segundoDado, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.primerDado, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cubilete As PictureBox
+    Friend WithEvents quintoDado As PictureBox
+    Friend WithEvents cuartoDado As PictureBox
+    Friend WithEvents tercerDado As PictureBox
+    Friend WithEvents segundoDado As PictureBox
+    Friend WithEvents primerDado As PictureBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ArchivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ JuegoDadosClase5 VB/Form1.vb
@@ -0,0 +1,91 @@
+﻿Public Class Form1
+    Dim Azar As Boolean = True
+    Dim vprimerDado As Integer = 0
+    Dim vsegundoDado As Integer = 0
+    Dim vtercerDado As Integer = 0
+    Dim vcuartoDado As Integer = 0
+    Dim vquintoDado As Integer = 0
+    Dim jugada As Integer = 1
+    Dim jugador As Integer = 1
+
+    Function NumDado(Random As Boolean) As Integer
+        If Random Then Randomize()
+        Return Int(Rnd() * 6 + 1)
+    End Function
+
+    Sub CambiaDado(ByRef DadoX As PictureBox, ByRef valorDado As Integer)
+        Dim num As Integer = NumDado(True) : valorDado = num
+        Select Case num
+            Case 1 : DadoX.Image = My.Resources.dado1
+            Case 2 : DadoX.Image = My.Resources.dado2
+            Case 3 : DadoX.Image = My.Resources.dado3
+            Case 4 : DadoX.Image = My.Resources.dado4
+            Case 5 : DadoX.Image = My.Resources.dado5
+            Case 6 : DadoX.Image = My.Resources.dado6
+        End Select
+
+    End Sub
+
+    Private Sub cubilete_Click(sender As Object, e As EventArgs) Handles cubilete.Click
+        Dim valorDado As Integer = 0
+
+        CambiaDado(primerDado, valorDado) : vprimerDado = valorDado
+        CambiaDado(segundoDado, valorDado) : vsegundoDado = valorDado
+        CambiaDado(tercerDado, valorDado) : vtercerDado = valorDado
+        CambiaDado(cuartoDado, valorDado) : vcuartoDado = valorDado
+        CambiaDado(quintoDado, valorDado) : vquintoDado = valorDado
+
+
+        If vprimerDado = vsegundoDado And vprimerDado = vtercerDado And vprimerDado = vcuartoDado And vprimerDado = vquintoDado Then 'Comparador en para sacar generala
+            MessageBox.Show("¡GENERALA!")                                              'Caso contrario se pasa con ElseIf a POKER
+        ElseIf vprimerDado = vsegundoDado And vprimerDado = vtercerDado And vprimerDado = vcuartoDado Then ' casos para POKER
+            MessageBox.Show("¡POKER!")
+        ElseIf vprimerDado = vtercerDado And vprimerDado = vcuartoDado And vprimerDado = vquintoDado Then 'Casos para POKER
+            MessageBox.Show("¡POKER!")
+        ElseIf vprimerDado = vsegundoDado And vprimerDado = vcuartoDado And vprimerDado = vquintoDado Then 'Casos para POKER
+            MessageBox.Show("¡POKER")   
+        ElseIf vprimerDado = vsegundoDado And vprimerDado = vtercerDado And vprimerDado = vquintoDado Then 'Casos para POKER
+            MessageBox.Show("¡POKER!")
+        ElseIf vsegundoDado = vtercerDado And vsegundoDado = vcuartoDado And vsegundoDado = vquintoDado Then 'Casos para POKER
+            MessageBox.Show("¡POKER!")
+        ElseIf vprimerDado = vsegundoDado And vprimerDado = vtercerDado Then          'En caso que no sea ninguno de los anteriores, pasa con elseif al FULL
+            MessageBox.Show("¡FULL!")
+        ElseIf vprimerDado = vcuartoDado And vprimerDado = vquintoDado Then
+            MessageBox.Show("¡FULL!")
+        ElseIf vprimerDado = vtercerDado And vprimerDado = vcuartoDado Then
+            MessageBox.Show("¡FULL!")
+        ElseIf vprimerDado = vsegundoDado And vprimerDado = vquintoDado Then
+            MessageBox.Show("¡FULL!")
+        ElseIf vprimerDado = vcuartoDado And vprimerDado = vsegundoDado Then
+            MessageBox.Show("¡FULL!")
+        ElseIf vprimerDado = vtercerDado And vprimerDado = vquintoDado Then
+            MessageBox.Show("¡FULL!")
+        ElseIf vsegundoDado = vtercerDado And vsegundoDado = vcuartoDado Then
+            MessageBox.Show("¡FULL!")
+        ElseIf vsegundoDado = vtercerDado And vsegundoDado = vquintoDado Then
+            MessageBox.Show("¡FULL!")
+        ElseIf vsegundoDado = vcuartoDado And vsegundoDado = vquintoDado Then
+            MessageBox.Show("¡FULL!")
+        ElseIf vcuartoDado = vtercerDado And vcuartoDado = vquintoDado Then
+            MessageBox.Show("¡FULL!")
+        End If
+        turnoPartida()
+    End Sub
+    Public Sub turnoPartida()
+        If jugador = 2 Then 'corrobora la cantidad de jugadas que hizo el jugador para cambiar el turno
+            MessageBox.Show("Turno Jugador 2")
+        Else If jugador = 4 then
+            jugador -= 4
+            MessageBox.Show("Turno Jugador 1")
+        End If
+        jugador+=1
+    End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
